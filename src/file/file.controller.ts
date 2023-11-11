6facafe6c42c305cab8ebe692b32b3dca7af0347
uriashnyk.vitalii <vuryashnik@huspi.com>
2023-11-11 22:51:38 +0100

    add file module

---
+++ src/file/file.controller.ts
@@ -0,0 +1,25 @@
+import {
+    Controller,
+    HttpCode,
+    Post,
+    Query,
+    UploadedFile,
+    UseInterceptors,
+} from '@nestjs/common';
+import { FileService } from "./file.service";
+import {Auth} from "../auth/decorators/auth.decorator";
+import {FileInterceptor} from "@nestjs/platform-express";
+
+@Controller('file')
+export class FileController {
+    constructor(private readonly fileService: FileService) {}
+
+    @Post()
+    @HttpCode(200)
+    @Auth('admin')
+    @UseInterceptors(FileInterceptor('image'))
+    async uploadFile(@UploadedFile() file: Express.Multer.File, @Query('folder') folder?: string) {
+        return this.fileService.saveFiles([file], folder);
+    }
+
+}
